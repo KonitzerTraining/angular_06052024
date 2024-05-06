2300d4515398b95b8ffa47b0ca7158800d9a0e43
Marcus Konitzer <mk@konitzer-training.de>
2024-05-06 12:06:46 +0200

    bootstrap

--- crm/src/app/app.component.ts
+++ crm/src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <div class="container my-4">
+    <h1 class="display-3">CRM</h1>
+    </div>
+  `,
 })
 export class AppComponent {
   title = 'crm';
