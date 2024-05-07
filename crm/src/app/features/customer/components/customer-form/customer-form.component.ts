20b93de4a3efdb8765a7eff7b7244a69767ddf51
Marcus Konitzer <mk@konitzer-training.de>
2024-05-07 13:52:00 +0200

    validierung

--- crm/src/app/features/customer/components/customer-form/customer-form.component.ts
+++ crm/src/app/features/customer/components/customer-form/customer-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-customer-form',
@@ -6,10 +7,30 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class CustomerFormComponent {
 
+  customerForm = this.fb.group({
+    name: [
+      '',
+      [
+        Validators.required,
+        Validators.minLength(2)
+      ]
+    ],
+    credit: [
+      0,
+      [
+        Validators.required,
+        Validators.min(0),
+      ]
+    ],
+  });
+
   @Output()
   formSubmit = new EventEmitter();
 
+
+  constructor(private fb: FormBuilder) {}
+
   formSubmitHandler() {
-    console.log('submit');
+    console.log(this.customerForm.value);
   }
 }
